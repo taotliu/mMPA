6b7e78a15d1d6492420de623a70d502e21d9929e
taotliu <tliu@stat.brown.edu>
2017-02-16 11:03:39 -0500

    add new files to test

---
+++ R/mMPA.R
@@ -0,0 +1,15 @@
+#' mMPA: A package for implementing marker-assisted mini-pooling
+#' with algorithm
+#'
+#' The \code{mMPA} package provides three categories of important functions:
+#' foo, bar and baz.
+#'
+#' Add some details hers (take from paper abstract)
+#'
+#' @section  functions:
+#' The foo functions ...
+#'
+#'
+#' @docType package
+#' @name mMPA
+NULL
